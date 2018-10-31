b56205143b540b67da1798e2fc6cdde5bbfb7957
adding table <zangjiahou@hotmail.com>
2018-10-31 17:46:36 -0400

    50 users inserted and authentication finished in previous steps

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -62,7 +62,7 @@
                                     </a>
 
                                     <a class="dropdown-item" href="{{ route('contact') }}">contact</a>
-                                    
+
                                             <a class="dropdown-item" href="{{ route('about') }}">about</a>
 
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
