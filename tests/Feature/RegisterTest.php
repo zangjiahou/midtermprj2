95d8ce47c2f7b2e28587a1e75f80ea589a1b001b
adding table <zangjiahou@hotmail.com>
2018-11-01 15:56:11 -0400

    register test successful

---
+++ tests/Feature/RegisterTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class RegisterTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testBasicTest()
+    {
+        $response = $this->get('/register');
+
+        $response->assertStatus(200);
+    }
+}
