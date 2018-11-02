fb79b87d99f2dd139b637d0d89c9b1a85385a60c
adding table <zangjiahou@hotmail.com>
2018-11-01 20:44:12 -0400

    unit test to update the name of a user in the database to Steve Smith

---
+++ tests/Unit/UpdateUserTest.php
@@ -0,0 +1,20 @@
+<?php
+namespace Tests\Unit;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+Use App\User;
+class UpdateUserTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testUpdateUser()
+    {
+        $user = User::find(1);
+        $user->name = 'Steve Smith';
+        $this->assertTrue($user->save());
+    }
+}
